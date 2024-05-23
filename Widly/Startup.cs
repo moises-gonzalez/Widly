17bb0c97f698d0042069a958b4f06dfdb1032284
moises.gonzalez@simpat.tech <moises.gonzalez@simpat.tech>
2024-05-22 19:20:25 -0600

    Add project files.

---
+++ Widly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Widly.Startup))]
+namespace Widly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
