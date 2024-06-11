343173a5d87c7c2d2f51d211d9050905cc1e1bca
moises.gonzalez@simpat.tech <moises.gonzalez@simpat.tech>
2024-06-10 18:08:56 -0600

    Adding validation messages to New Customer Form

--- Widly/Controllers/CustomersController.cs
+++ Widly/Controllers/CustomersController.cs
@@ -37,6 +37,16 @@ namespace Widly.Controllers
         [HttpPost]
         public ActionResult Save (Customer customer) 
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
