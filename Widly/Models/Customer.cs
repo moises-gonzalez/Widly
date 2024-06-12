57848dc39a1a5e60968131405f629d0601a04e37
moises.gonzalez@simpat.tech <moises.gonzalez@simpat.tech>
2024-06-11 18:29:43 -0600

    Adding customer validations on Customer

--- Widly/Models/Customer.cs
+++ Widly/Models/Customer.cs
@@ -23,6 +23,7 @@ namespace Widly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
---
+++ Widly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Widly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
+        }
+    }
+}
