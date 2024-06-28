eb56c1c4ac795ba0eb1b08c9d2ee3c8f897065c9
moises.gonzalez@simpat.tech <moises.gonzalez@simpat.tech>
2024-06-27 19:12:21 -0600

    Trying to install AutoMapper

---
+++ Widly/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Widly.Models;
+
+namespace Widly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Required]
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
